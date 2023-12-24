0397b61663446e23da2916311b7e5d4c924fc6c3
andy2022ey <andy.eyyl@gmail.com>
2023-12-24 21:13:32 +0800

    add

--- article/serializers.py
+++ article/serializers.py
@@ -4,4 +4,4 @@ from .models import Item
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
-        fields = ['id', 'title']
+        fields = ('id', 'title', 'tag', 'body', 'created_at', 'updated_at')
